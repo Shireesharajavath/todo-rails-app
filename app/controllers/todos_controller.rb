cbe3f11b78db9b6317382c42735d458e183e0600
Srajavath <shireshar1@gmail.com>
2025-09-16 11:07:31 +0530

    Initial commit: Rails todo_app project

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,64 +1,47 @@
-
 class TodosController < ApplicationController
+ 
+  skip_before_action :verify_authenticity_token
+
   before_action :authenticate_user
-  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo, only: [:show, :update, :destroy]
 
+  
   def index
-    @todos = current_user.todos
-    respond_to do |format|
-      format.html
-      format.json { render json: @todos }
-    end
+    todos = current_user.todos
+    render json: todos
   end
 
+  
   def show
-    respond_to do |format|
-      format.html
-      format.json { render json: @todo }
-    end
+    render json: @todo
   end
 
-  def new
-    @todo = current_user.todos.new
-  end
-
+ 
   def create
-    @todo = current_user.todos.new(todo_params)
-    if @todo.save
-      respond_to do |format|
-        format.html { redirect_to todos_path, notice: "Todo created successfully." }
-        format.json { render json: @todo, status: :created }
-      end
+    todo = current_user.todos.new(todo_params)
+    if todo.save
+      render json: { message: "Todo created successfully", todo: todo }, status: :created
     else
-      respond_to do |format|
-        format.html { render :new }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
+      render json: { errors: todo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
-
+  
   def update
     if @todo.update(todo_params)
-      respond_to do |format|
-        format.html { redirect_to todos_path, notice: "Todo updated successfully." }
-        format.json { render json: @todo }
-      end
+      render json: { message: "Todo updated successfully", todo: @todo }, status: :ok
     else
-      respond_to do |format|
-        format.html { render :edit }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
+      render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
+  
   def destroy
-    @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_path, notice: "Todo deleted." }
-      format.json { head :no_content }
+    if @todo
+      @todo.destroy
+      render json: { message: "Todo deleted successfully" }, status: :ok
+    else
+      render json: { error: "Todo not found or unauthorized" }, status: :not_found
     end
   end
 
@@ -67,10 +50,8 @@ class TodosController < ApplicationController
   def set_todo
     @todo = current_user.todos.find_by(id: params[:id])
     unless @todo
-      respond_to do |format|
-        format.html { redirect_to todos_path, alert: "Todo not found or unauthorized." }
-        format.json { render json: { error: "Todo not found or unauthorized" }, status: :unauthorized }
-      end
+      render json: { error: "Todo not found or unauthorized" }, status: :not_found
+      return 
     end
   end
 
@@ -86,10 +67,8 @@ class TodosController < ApplicationController
     )
   end
 
-
   def authenticate_user
     auth_header = request.headers["Authorization"]
-    puts auth_header
     if auth_header.present? && auth_header =~ /^Token token=(.+)$/
       token = Regexp.last_match(1)
       @current_user = User.find_by(api_key: token)
@@ -98,10 +77,8 @@ class TodosController < ApplicationController
     end
 
     unless @current_user
-      respond_to do |format|
-        format.html { redirect_to login_path, alert: "You must be logged in." }
-        format.json { render json: { error: "Unauthorized" }, status: :unauthorized }
-      end
+      render json: { error: "Unauthorized" }, status: :unauthorized
+      return 
     end
   end
 
